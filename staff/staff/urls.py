aca59ec4019a81e607ffabc56a19501a89d89c24
bin wu <ben.w.dbm@gmail.com>
2017-06-16 09:24:52 +0800

    orders for staff panel

--- staff/staff/urls.py
+++ staff/staff/urls.py
@@ -18,10 +18,12 @@ from django.conf.urls import url
 from django.conf.urls import include
 from common.apps.users.urls import staff_urlpatterns as user_urls
 from . import views
-
+from common.apps.orders import urls_staff as ordersurl
 
 urlpatterns = [
     #url(r'^admin/', admin.site.urls),
     url(r'^$', views.home, name='home'),
     url(r'^users/', include((user_urls, 'users'))),
+    #orders
+    url(r'^orders/', include(ordersurl, namespace="orders")),
 ]
