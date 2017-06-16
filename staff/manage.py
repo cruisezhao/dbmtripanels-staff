5db8c8cac90f7c0e7f50c4b326b0cb712e137637
adolf.y <adolf.y.dbm@gmail.com>
2017-06-16 10:06:22 +0800

    staff样式统一

--- staff/manage.py
+++ staff/manage.py
@@ -9,7 +9,7 @@ except ImportError:
     pass
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "staff.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "staff.local_settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
--- staff/staff/views.py
+++ staff/staff/views.py
@@ -6,6 +6,6 @@ Created on Jun 13, 2017
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-@login_required
+
 def home(request):
     return render(request,"home.html")
