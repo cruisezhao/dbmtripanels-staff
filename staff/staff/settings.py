79f9c562d990a52106925c9aa70706597ae55a96
bin wu <ben.w.dbm@gmail.com>
2017-06-23 16:14:44 +0800

    cross domain cookie

--- staff/staff/settings.py
+++ staff/staff/settings.py
@@ -25,7 +25,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '3@+wn)qdzs3i@u#rc9@jlw__n7u@3y)c#2x#$%9fhln3-70s@s'
+#SECRET_KEY = '3@+wn)qdzs3i@u#rc9@jlw__n7u@3y)c#2x#$%9fhln3-70s@s'
+SECRET_KEY = 'f5l(t&w_k34o%zg!od8h+3f11by!v_)l*jydv#(t986mi27swu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
